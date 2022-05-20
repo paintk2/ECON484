19412f4bc1e4f8b350617035f228f60a28d6ac27
paintk2 <77028804+paintk2@users.noreply.github.com>
2022-05-20 13:15:59 -0700

    Add files via upload

--- final_analysis.R
+++ final_analysis.R
@@ -2,7 +2,26 @@
 # ECON 484 
 # Group 4 
 
+# Packages 
+library(tidyverse)
+
 # Data Wrangling 
+
+sales <- read.csv("rp_sales.csv")
+
+#change document date to year/date format 
+
+sales_clean <- sales %>% 
+  filter(PropertyType %in% c(10, 11, 12, 13, 14, 18, 19, 2, 3, 6)) %>% 
+  mutate(DocumentDate = as.numeric(substr(DocumentDate, 7, 10))) 
+
+sales_2012 <- sales_clean %>% 
+  filter(DocumentDate >= 2012)
+  
+  
+  # not including mobile homes, check 6 
+
+
 
 # Feature Selection 
 
