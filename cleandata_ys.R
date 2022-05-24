ab6f58ccac99ec71083dd3d7943e5fc57027069a
Eakin8 <78818274+Eakin8@users.noreply.github.com>
2022-05-23 17:32:16 -0700

    Update cleandata_ys.R

--- cleandata_ys.R
+++ cleandata_ys.R
@@ -7,22 +7,27 @@ require(readr)
 # combine major and minor key
 # delete columns BldgNbr, FirstBillYr, LastBillYr, ValuedBy, UpdatedBy, UpdateDate
 # remove rows with 0 improvement value
+# factor data
 
-# homeexe <- read.csv('EXTR_HomeImpExempts.csv')
-# 
-# homeexe <- homeexe %>%
-#   mutate(mm_key = paste0(Major, '-', Minor), ValueDate = substr(ValueDate, 1, 4), .before = 1) %>%
-#   select(-c(Major, Minor, BldgNbr, FirstBillYr, LastBillYr, ValuedBy, UpdatedBy, UpdateDate))
-# 
-# # filter those with an actual improvement value
-# homeexe <- homeexe %>%
-#   filter(HomeImpVal > 0)
-# 
-# 
-# write.csv(homeexe, 'EXTR_HomeImpExempts.csv', row.names = FALSE)
+homeexe <- read.csv('EXTR_HomeImpExempts.csv')
+
+homeexe <- homeexe %>%
+  mutate(mm_key = paste0(Major, '-', Minor), ValueDate = substr(ValueDate, 1, 4), .before = 1) %>%
+  select(-c(Major, Minor, BldgNbr, FirstBillYr, LastBillYr, ValuedBy, UpdatedBy, UpdateDate))
+
+# filter those with an actual improvement value
+homeexe <- homeexe %>%
+  filter(HomeImpVal > 0)
+
+# factor the data
+homeexe$HIExemptId = as.factor(homeexe$HIExemptId)
+homeexe$NoteId = as.factor(homeexe$NoteId)
+
+write.csv(homeexe, 'EXTR_HomeImpExempts.csv', row.names = FALSE)
 
 # clean EXTR_ValueHistory_V.csv
 # combine major and minor
+# factor data
 
 valhist <- read.csv('EXTR_ValueHistory_V.csv')
 
@@ -30,10 +35,15 @@ valhist <- valhist %>%
   mutate(mm_key = paste0(Major, '-', Minor), .before = 1) %>%
   select(-c(Major, Minor, OmitYr, ApprLandVal, ApprImpsVal, ApprImpIncr, TaxValReason, TaxStatus, ChangeDate, ChangeDocId, Reason, SplitCode))
 
+# filter those that are prior to 2012
 valhist <- valhist %>%
   filter(TaxYr > 2012)
 
+# factor data
+valhist$LevyCode = as.factor(valhist$LevyCode)
+
 write.csv(valhist, 'EXTR_ValueHistory_V.csv', row.names = FALSE)
+
 
 
 
